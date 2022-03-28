f2b11bfb74ab1df471c61898846de65b085e99c4
Scott St Germain <scottjustin5000@gmail.com>
2022-03-28 15:15:42 -0700

    update selection events

--- src/components/TestComponent.stories.js
+++ src/components/TestComponent.stories.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react-native'
-
-import TestComponent from './TestComponent'
-import Checkbox from './checkbox'
 import MultiSelect from './multiselect'
 
-storiesOf('Test Component', module).add('example', () => <MultiSelect />)
+const data = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+      label: 'First Item'
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+      label: 'Second Item'
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+      label: 'Third Item'
+    },
+  ];
+
+  const handleSubmit = (selected) => {
+    console.log('SELECTED', selected)
+  }
+
+storiesOf('Test Component', module).add('example', () => <MultiSelect onSubmit={handleSubmit} data={data} title='Select something...' />)
